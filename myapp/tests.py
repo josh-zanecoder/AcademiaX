9bfa56c0d2b8a25c62a635eed8b75c9a3fb1f693
josh-zanecoder <josh@zanecoder.com>
2025-01-31 16:47:47 +0800

    Student Registration UI

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+
+
+
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        # Initialize the test client
+        self.client = Client()
+    
+    def test_index_view(self):
+        # Test the index view
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+    
+    def test_registration_view(self):
+        # Test the registration view
+        response = self.client.get(reverse('registration'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'students/registration.html')
